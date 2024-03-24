f3a520944254896148cf513296d58d6e25cd3df7
callikys <callikys@naver.com>
2024-03-25 01:10:23 +0900

    devTogether Server 프로젝트 파일 생성

---
+++ src/main/java/yiu/aisl/devTogether/DevTogetherApplication.java
@@ -0,0 +1,13 @@
+package yiu.aisl.devTogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yiu/aisl/devTogether/DevTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package yiu.aisl.devTogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
