156d17009df437cff8dee94c9780d14ec6fac576
sj0404k <ksj06030@naver.com>
2024-05-08 14:13:21 +0900

    fix: domain 연결 수정

---
+++ src/main/java/yiu/aisl/devTogether/dto/BoardRequestDto.java
@@ -0,0 +1,59 @@
+package yiu.aisl.devTogether.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class BoardRequestDto {
+    @Getter
+    @Setter
+    public class DetailDto{
+        private Long boardID;
+    }
+    @Getter
+    @Setter
+    public static class CreateDto{
+        private String title;
+        private String contents;
+        private String file;
+        private Long userId;
+    }
+    @Getter
+    @Setter
+    public class DeleteDto{
+        private Long boardId;
+
+    }
+    @Getter
+    @Setter
+    public class UpdateDto{
+        private Long boardId;
+        private String title;
+        private String contents;
+        private String file;
+    }
+    @Getter
+    @Setter
+    public class likeDto {
+        private Long boardId;
+        private Long likeId;
+        private Boolean count;
+        //private int type;
+    }
+    @Getter
+    @Setter
+    public class CreateCommentDto {
+        private Long boardId;
+        private String contents;
+    }
+    @Getter
+    @Setter
+    public class DeleteCommentDto{
+        private Long boardId;
+        private Long CommentId;
+    }
+    @Getter
+    @Setter
+    public class CreatScrapDto{
+        private Long boardId;
+    }
+}
