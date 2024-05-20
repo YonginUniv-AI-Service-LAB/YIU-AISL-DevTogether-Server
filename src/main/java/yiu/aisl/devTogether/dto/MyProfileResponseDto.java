f040214b23e4e889b6a484850da2edf2b7d12e9d
callikys <callikys@naver.com>
2024-05-20 22:40:11 +0900

    feat: 내 정보 조회/수정 api 제작

---
+++ src/main/java/yiu/aisl/devTogether/dto/MyProfileResponseDto.java
@@ -0,0 +1,31 @@
+package yiu.aisl.devTogether.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyProfileResponseDto {
+    private String email;
+    private String name;
+    private String nickname;
+    private Integer role;
+    private Integer gender;
+    private Integer age;
+    private String location1;
+    private String location2;
+    private String location3;
+    private String subject1;
+    private String subject2;
+    private String subject3;
+    private String subject4;
+    private String subject5;
+    private String method;
+    private Integer fee;
+    private MultipartFile img;
+}
