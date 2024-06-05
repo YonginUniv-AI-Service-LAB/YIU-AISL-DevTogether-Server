f2623045f118464aaae86d228d204c289a1a7825
callikys <callikys@naver.com>
2024-06-05 11:32:11 +0900

    feat: 내 멘토 프로필 조회 및 내 멘티 프로필 조회 제작

---
+++ src/main/java/yiu/aisl/devTogether/dto/UserProfileResponseDto.java
@@ -0,0 +1,43 @@
+package yiu.aisl.devTogether.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import yiu.aisl.devTogether.domain.UserProfile;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileResponseDto {
+    private Long id;
+    private String introduction;
+    private String pr;
+    private String link;
+    private String contents;
+    private String schedule;
+    private String method;
+    private Boolean img;
+    private Integer fee;
+    private Boolean files;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public UserProfileResponseDto(UserProfile userProfile) {
+        this.id = userProfile.getUserProfileId();
+        this.introduction = userProfile.getIntroduction();
+        this.pr = userProfile.getPr();
+        this.link = userProfile.getLink();
+        this.contents = userProfile.getContents();
+        this.schedule = userProfile.getSchedule();
+        this.method = userProfile.getMethod();
+        this.img = userProfile.getImg();
+        this.fee = userProfile.getFee();
+        this.files = userProfile.getFiles();
+        this.createdAt = userProfile.getCreatedAt();
+        this.updatedAt = userProfile.getUpdatedAt();
+    }
+}
